4d745e5dbf2e578903328c58bc06e254f007065f
JamesMangahas <Jamesmangahas1988@gmail.com>
2026-06-05 19:54:29 +0800

    calculator

---
+++ calculator.js
@@ -0,0 +1,16 @@
+function add(a,b){
+    return a + b
+}
+function subtraction(a,b){
+    return a - b
+}
+function multiplication(a,b){
+    return a * b
+}
+function division(a,b){
+    return a / b
+}
+console.log(add(5, 7));
+console.log(subtraction(2, 7));
+console.log(multiplication(1, 7));
+console.log(division(500,5));
